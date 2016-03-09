b768ee2f7d488de6db74d6a10c342afb9738584f
Sergey Solomatin <sergeysolomatin@Sergeys-MacBook-Air.local>
2016-03-09 18:26:55 +0300

    Edited TinyHttpd program. Added index.html

--- Java/TinyHttpd/TinyHttpd.java
+++ Java/TinyHttpd/TinyHttpd.java
@@ -4,18 +4,19 @@
 
 import java.net.*;
 import java.io.*;
+import java.util.regex.*;
 import java.util.concurrent.*;
 
 public class TinyHttpd {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Executor executor = Executors.newFixedThreadPool(3);
-		ServerSocket serverSocket = new ServerSocket(Integer.parseInt(argv[0]));
+		ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
 		while ( true )
 			executor.execute(new TinyHttpdConnection( serverSocket.accept() ) );
 	}
 }
 
-public class TinyHttpdConnection implements Runnable {
+class TinyHttpdConnection implements Runnable {
 	Socket client;
 	
 	TinyHttpdConnection( Socket client ) {
@@ -29,6 +30,33 @@ public class TinyHttpdConnection implements Runnable {
 			PrintWriter pout = new PrintWriter(new OutputStreamWriter(out, "8859_1"), true);
 			String request = in.readLine();
 			System.out.println("Request: " + request);
-		}	
+			
+			if(null != request) {			
+			Matcher get = Pattern.compile("GET /?(\\S*).*").matcher( request );
+			
+			if( get.matches() ) {
+				request = get.group(1);
+				if (request.endsWith("/") || request.equals(""))
+					request = request + "index.html";
+				try {
+					FileInputStream fis = new FileInputStream( request );
+					byte [] data = new byte[ 64 * 1024 ];
+					for(int read; (read = fis.read( data )) > -1; ) {
+						out.write( data, 0, read);
+					}
+					out.flush();
+				}
+				catch( FileNotFoundException e ) {
+					pout.println("404 Not Found");
+				}
+			} else
+				pout.println("400 Bad Request");
+			}
+			client.close();
+			
+		}
+		catch ( IOException e ) {
+			System.out.println( "I/O Error: " + e );
+		}
 	}
 }
